4582bf3d8b8c236f8510242b21b2fc17e9b47301
Misha Fedorov <m.fedorov20021@gmail.com>
2021-12-17 06:48:19 +0300

    Update apiKey

--- PlanetNews/PlanetNews/Core/Entitites/NewsApiOrg.swift
+++ PlanetNews/PlanetNews/Core/Entitites/NewsApiOrg.swift
@@ -5,5 +5,6 @@ struct NewsApiOrg {
     // 705e24c51b36484b97b0877baaf71a77
     // 8596813ff49c4b0bb94919e42acc7918
     // f01d91e9599b452796fa0cbdd5fc1150
-    static let apiKey = "61efabb7499840899a3e8f4d1542e12e"
+    // 10b837824efb4218bdfc089456fd7649
+    static let apiKey = "10b837824efb4218bdfc089456fd7649"
 }
